16fdf6424439cf9da6c8ec652d9730b98efc23a5
Advay Sagarkar <72992475+Aragorn-64@users.noreply.github.com>
2022-01-21 11:51:03 +0000

    Added db functionality + Setup a help command

--- main.py
+++ main.py
@@ -6,24 +6,21 @@ from replit import db
 client=discord.Client()
 
 #defaults, incase users wish to reset
-default_hello_inputs = ["Hello", "hello", "Henlo", "henlo"]
+default_hello_inputs = ["%Hello", "%hello", "%Henlo", "%henlo"]
 default_hello_outputs = ["Hello", "hello", "Henlo", "henlo", ]
 default_smiles = [":grinning:", ":smiley:", ":slight_smile:", ":smile:", ":grin:"]
 
-starter_hello_inputs = ["Hello", "hello", "Henlo", "henlo"]
+starter_hello_inputs = ["%hello", "%henlo"]
 starter_hello_outputs = ["Hello", "hello", "Henlo", "henlo", ]
 starter_smiles = [":grinning:", ":smiley:", ":slight_smile:", ":smile:", ":grin:"]
 
-def update_db(key, value):
+def update_db(key, value): #updating "key" db with "value"
   if key in db.keys():
     db1 = db[key]
     db1.append(value)
     db[key] = db1
   else:
     db[key] = [value]
-
-
-
 
 @client.event
 async def on_ready():
@@ -35,15 +32,31 @@ async def on_message(message):
     return
 
   msg = message.content
+  if "hello in" in db.keys():
+    in_options = starter_hello_inputs + db["hello in"]
+  else:
+    in_options = starter_hello_inputs
 
-  if msg.startswith('#hello'):
-    #Replies to the #hello with Author username or nickname if set + a random smile:
+  #basic help guide
+  if msg=="%help":
+    await message.channel.send("""`ps-bot guide: \nGreet me ("%help inputs" to see the list of valid inputs) and I will reply ("-" to see a list of my replies)`""")
+
+  if msg == "%help inputs":
+    await message.channel.send("`ps-bot inputs:`")
+    i = 1
+    for x in in_options:
+      await message.channel.send("`" + str(i) + ". " + x + ",`")
+      i += 1
+    await message.channel.send("`use - to add new inputs, and - to delete an input`")
+
+
+  if msg in in_options:
+    #Replies to the %hello with Author username or nickname if set + a random smile:
     author_nick = str(message.author.nick)
     if author_nick == 'None':
       author = str(message.author)
-      author = author.split('#')
-      await message.channel.send( random.choice(starter_hello_outputs) + " " + author[0] + " " + random.choice(starter_smiles))
-      
+      author = author.split('%')
+      await message.channel.send( random.choice(starter_hello_outputs) + " " + author[0] + " " + random.choice(starter_smiles)) 
     else:
       await message.channel.send(random.choice(starter_hello_outputs) + " " + author_nick + " " + random.choice(starter_smiles))
     
