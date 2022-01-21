f4854c2879014c242b1cc9bc8e1ab40d157cbfd9
Advay Sagarkar <72992475+Aragorn-64@users.noreply.github.com>
2022-01-21 12:20:53 +0000

    modified

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 import os
 import random
 from replit import db
+from collections import OrderedDict
 
 client=discord.Client()
 
@@ -22,6 +23,9 @@ def update_db(key, value): #updating "key" db with "value"
   else:
     db[key] = [value]
 
+#def delete_db(key, value):
+
+
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
@@ -32,23 +36,31 @@ async def on_message(message):
     return
 
   msg = message.content
+  in_options = starter_hello_inputs 
   if "hello in" in db.keys():
-    in_options = starter_hello_inputs + db["hello in"]
-  else:
-    in_options = starter_hello_inputs
-
+    in_options.extend(db["hello in"])
+  
   #basic help guide
   if msg=="%help":
     await message.channel.send("""`ps-bot guide: \nGreet me ("%help inputs" to see the list of valid inputs) and I will reply ("-" to see a list of my replies)`""")
 
   if msg == "%help inputs":
     await message.channel.send("`ps-bot inputs:`")
-    i = 1
-    for x in in_options:
-      await message.channel.send("`" + str(i) + ". " + x + ",`")
-      i += 1
+    in_options = list(OrderedDict.fromkeys(in_options))
+    await message.channel.send("`" + str(in_options) + "`")
+    #i = 1
+    #for x in in_options:
+    #  await message.channel.send("`" + str(i) + ". " + x + ",`")
+    #  i += 1
     await message.channel.send("`use - to add new inputs, and - to delete an input`")
 
+  if msg.startswith("%new input"):
+    new_input = msg.split("%new input ",1)[1]
+    if new_input in db["hello in"]:
+      await message.channel.send("Input is already recognized")
+    else:  
+      update_db("hello in",new_input)
+      await message.channel.send(new_input + " added to list of valid inputs")
 
   if msg in in_options:
     #Replies to the %hello with Author username or nickname if set + a random smile:
